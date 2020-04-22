14e08bfc24be6e8fb924b0099a3414270f04929e
Totoro-n119 <2356435502@qq.com>
2020-04-22 20:46:22 +0800

    限制540大小

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-home">
-    <h1>首页</h1>
+    <header class="index-header">
+      <a href="mine.html?cpid=0">
+        <div class="header-user">
+          <div class="user-btn"></div>
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="search.html?cpid=0">
+        <div class="header-search"></div>
+      </a>
+    </header>
   </div>
 </template>
 
@@ -9,3 +19,43 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+
+  .page-home {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .index-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 20px;
+      height: 44px;
+      box-sizing: border-box;
+      // border-bottom: 1px solid #e9e9e9;
+      .user-btn {
+        width: 25px;
+        height: 25px;
+        background: url("../../assets/icon/user-btn.png") no-repeat;
+        background-size: 100%;
+      }
+      .header-logo {
+        width: 92px;
+        height: 28px;
+        background: url("../../assets/logo.png") no-repeat;
+        background-size: 100%;
+      }
+      .header-search {
+        width: 25px;
+        height: 25px;
+        background: url("../../assets/icon/search.png") no-repeat;
+        background-size: 100%;
+      }
+    }
+    .my-swiper img {
+      width: 100%;
+    }
+  }
+</style>
